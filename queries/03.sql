304bedc1dd8edf2a7540a085359802ce3861e40b
2023Christine <asaic2023@gmail.com>
2024-02-16 12:22:22 -0500

    Added visualization

--- queries/03.sql
+++ queries/03.sql
@@ -1,6 +1,6 @@
 -- Create a new genre called "Allegory"
-INSERT INTO Genres (GenreName, Description)
-VALUES ('Allegory', 'A form of expression in which elements signify meanings other than their literal sense.');
+INSERT INTO Genres (GenreID, GenreName, Description)
+VALUES (11,'Allegory', 'A form of expression in which elements signify meanings other than their literal sense.');
 
 -- Update the genre of 'Animal Farm' to 'Allegory'
 UPDATE Books
