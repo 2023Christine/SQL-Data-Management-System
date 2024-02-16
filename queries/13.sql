7f82f2aa7b341b4810d89b914d0e4f254b9e0b6d
2023Christine <asaic2023@gmail.com>
2024-02-16 12:29:29 -0500

    edited 13

--- queries/13.sql
+++ queries/13.sql
@@ -4,9 +4,3 @@ FROM Genres g
 INNER JOIN Books b ON g.GenreID = b.GenreID
 GROUP BY g.GenreName
 HAVING COUNT(b.BookID) > 0;
-
--- Book availability
-SELECT s.StatusName, COUNT(b.BookID) AS NumberOfBooks
-FROM AvailabilityStatus s
-LEFT JOIN Books b ON s.StatusID = b.AvailabilityStatusID
-GROUP BY s.StatusName;
